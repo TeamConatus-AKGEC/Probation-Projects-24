e7754b69dae2a61e236be78de61fd1b44b83663d
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-10-20 00:11:41 +0530

    stats functionality added

--- Kushagra_agarwal/first.js
+++ Kushagra_agarwal/first.js
@@ -1,9 +1,13 @@
 const addBtn = document.querySelector("#add-button");
 const taskList = document.querySelector(".task-list");
-
+const numr = document.querySelector("#nr");
+const denr = document.querySelector("#dr");
+let count = 0; 
+let done = 0 
 
 addBtn.addEventListener("click",addTask)
 function addTask(){
+    
     const input = document.querySelector("#input");
     const taskValue = input.value;
 
@@ -11,6 +15,9 @@ function addTask(){
         alert("please enter a task");
     }
     else{
+        denr.textContent = ++count;
+        // console.log(count);
+        
         const listItem = document.createElement("li")
         const taskSpan = document.createElement("span")
 
@@ -25,9 +32,12 @@ function addTask(){
         dltBtn.setAttribute("id","delete-btn")
         dltBtn.textContent = "Delete";
         // dltBtn.style.marginLeft = "10px";
-        listItem.appendChild(dltBtn);
+        // listItem.appendChild(dltBtn);
         dltBtn.addEventListener("click", function (){
             listItem.remove();
+            denr.textContent = --count;
+            numr.textContent = --done;
+            dltBtn.disabled=true;
         })
 
 
@@ -35,10 +45,17 @@ function addTask(){
         const taskDone = document.createElement("button")
         taskDone.setAttribute("id","done-btn")
         taskDone.textContent = "Done";
-        listItem.appendChild(taskDone)
+        // listItem.appendChild(taskDone)
         taskDone.addEventListener("click", function(){
             taskSpan.style.textDecoration="line-through";
+            numr.textContent = ++done;
+            taskDone.disabled=true;
         })
+
+        const divBtn = document.createElement("div")
+        divBtn.setAttribute("id","button-container")
+        divBtn.append(dltBtn,taskDone);
+        listItem.appendChild(divBtn)
 
     }
     input.value = "";
