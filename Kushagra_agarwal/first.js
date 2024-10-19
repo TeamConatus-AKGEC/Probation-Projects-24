4a9fe89c26b50d07914bb08911cd793d3edfe0ad
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-10-19 21:58:09 +0530

    linethrough issue solved using span

--- Kushagra_agarwal/first.js
+++ Kushagra_agarwal/first.js
@@ -4,15 +4,43 @@ const taskList = document.querySelector(".task-list");
 
 addBtn.addEventListener("click",addTask)
 function addTask(){
-    const input = document.querySelector("#input")
+    const input = document.querySelector("#input");
     const taskValue = input.value;
+
     if (taskValue === ""){
         alert("please enter a task");
     }
     else{
         const listItem = document.createElement("li")
-        listItem.textContent = taskValue
+        const taskSpan = document.createElement("span")
+
+        taskSpan.textContent = taskValue
+        listItem.appendChild(taskSpan)
         const ul = document.querySelector(".task-list")
         ul.appendChild(listItem);
+
+
+
+        const dltBtn = document.createElement("button")
+        dltBtn.setAttribute("id","delete-btn")
+        dltBtn.textContent = "Delete";
+        // dltBtn.style.marginLeft = "10px";
+        listItem.appendChild(dltBtn);
+        dltBtn.addEventListener("click", function (){
+            listItem.remove();
+        })
+
+
+
+        const taskDone = document.createElement("button")
+        taskDone.setAttribute("id","done-btn")
+        taskDone.textContent = "Done";
+        listItem.appendChild(taskDone)
+        taskDone.addEventListener("click", function(){
+            taskSpan.style.textDecoration="line-through";
+        })
+
     }
+    input.value = "";
+
 };
