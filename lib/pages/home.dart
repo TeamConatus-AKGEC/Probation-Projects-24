c3e78db74246c3533792107ae80fd329d6d58c2e
Sneha-2804 <sagupta28.04@gmail.com>
2024-10-20 09:06:25 +0530

    Added a scrollable categories

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -32,24 +32,87 @@ class _HomeState extends State<Home> {
         ],
         bottom:  PreferredSize(
           preferredSize: Size.fromHeight(70.0),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search , color: Colors.grey,),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(40),
-                  borderSide: BorderSide.none,
+          child: Container(
+            child: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search here...',
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                          ),
+                          prefixIcon: Icon(Icons.search ,
+                            color: Colors.grey,),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.zero,
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                contentPadding: EdgeInsets.zero,
-                filled: true,
-                fillColor: Colors.white,
               ),
             ),
           ),
       ),
-      )
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+          child: showItem()),
+      );
+  }
+
+  Widget showItem() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _buildItem("images/fashion.jpeg", "Fashion"),
+          _buildItem("images/mobile.jpeg", "Mobile"),
+          _buildItem("images/grocery.jpeg", "Grocery"),
+          _buildItem("images/pharmacy.jpeg", "Pharmacy"),
+          _buildItem("images/travel.jpeg", "Travel"),
+          _buildItem("images/electronics.jpeg", "Electronics"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildItem(String imagePath, String label) {
+    return Material(
+      elevation: 2.0,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        padding: const EdgeInsets.all(8),
+        width: 80, // Adjust width as needed
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              imagePath,
+              height: 50,
+              width: 50,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 3), // Space between image and text
+            Text(
+              label,
+              style: TextStyle(fontSize: 12), // Adjust text style as needed
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widges/widges_support.dart
+++ lib/widges/widges_support.dart
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
+
 
 class AppWidges {
   static TextStyle boldTextFeildStyle() {
@@ -9,16 +9,4 @@ class AppWidges {
         fontWeight: FontWeight.bold,
         fontFamily: 'Playwrite');
   }
-=======
-import 'package:flutter/material.dart';
-
-class AppWidges {
-  static TextStyle boldTextFeildStyle() {
-    return const TextStyle(
-        color: Colors.black,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        fontFamily: 'Playwrite');
-  }
->>>>>>> 847496094d807fa4cc1e9aa3303f4f1e3d16026c
 }
