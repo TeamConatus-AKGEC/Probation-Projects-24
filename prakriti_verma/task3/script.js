c47200d3d4964669826d2d93d53807cfef136864
Prakriti <anukriti.1911@gmail.com>
2024-11-09 00:59:42 +0530

    zombies created and movement given

--- prakriti_verma/task3/script.js
+++ prakriti_verma/task3/script.js
@@ -7,6 +7,8 @@ const cellSize = 100; //size of each cell
 const cellGap = 3;
 const resources = 200; //initial value of the resources available to the player
 const plant = []; //for collection of plants
+const zombie = [];
+let frame = 0;
 
 //to keep track of movement of mouse over the grid
 const mouse = {
@@ -28,7 +30,7 @@ class plants{
     draw(){
         ctx.fillStyle = 'light green'; //color of plant
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.font = '20px Arial';
+        ctx.font = '30px Arial';
         ctx.fillText(Math.floor(this.health), this.x , this.y);
         ctx.fillStyle = 'gold'; //color of text inside
     }
@@ -45,7 +47,7 @@ canvas.addEventListener('click', function(){
         if(plant[i].x === positionX && plant[i].y === positionY)
             return;
     }
-    
+
     let plantCost = 60;
 
     //creation of an empty plant object based on resources available
@@ -61,6 +63,42 @@ function handlePlants(){
     }
 }
 
+class zombies{
+    constructor(verticalPos){
+        this.x = canvas.width;
+        this.y = verticalPos;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.speed = Math.random()* 0.2 + 0.4;
+        this.move = this.speed;
+        this.health = 90; //current health of a zombie
+        this.maxHealth = 90; //to keep track of initial health of every zombie
+    }
+    update(){
+        this.x -= this.movement;
+    }
+    draw(){
+        ctx.fillStyle = 'brown';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.font = '30px Arial';
+        ctx.fillText(Math.floor(this.health), 20, 55);
+        ctx.fillStyle = 'greenyellow';
+    }
+}
+
+function handleZombies(){
+    for(let i = 0 ; i < zombie.length ; i++)
+        {
+            zombie[i].update;
+            zombie[i].draw;
+        }
+        if(frame % 100 === 0)
+        {
+            let verticalPos = Math.floor(Math.random() * 5 + 1) * cellSize; //for random selection of row for zombie movement
+            zombie.push(new zombies(verticalPos));
+        }
+}
+
 function gameStatus(){
     fillStyle = 'gold';
     ctx.font = '30px Arial';
@@ -70,5 +108,6 @@ function gameStatus(){
 function animate(){
     handlePlants();
     gameStatus();
+    frame++;
 }
 animate();
