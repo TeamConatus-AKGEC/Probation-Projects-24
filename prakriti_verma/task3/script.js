5a4620be6e86fb09516e13b93fc0cb913b7e8d06
Prakriti <anukriti.1911@gmail.com>
2024-11-08 23:01:02 +0530

    for setting plants by clicking in the grid

--- prakriti_verma/task3/script.js
+++ prakriti_verma/task3/script.js
@@ -5,8 +5,15 @@ canvas.height = 600;
 
 const cellSize = 100; //size of each cell
 const cellGap = 3;
-
+const resources = 200; //initial value of the resources available to the player
 const plant = []; //for collection of plants
+
+//to keep track of movement of mouse over the grid
+const mouse = {
+    x: undefined,
+    y: undefined,
+}
+
 class plants{
     constructor(x, y){
         this.x = x;
@@ -18,4 +25,24 @@ class plants{
         this.projectiles = [];
         this.timer = 0;
     }
+    draw(){
+        ctx.fillStyle = 'light green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.font = '20px Arial';
+        ctx.fillText(Math.floor(this.health), this.x, this.y);
+    }
 }
+canvas.addEventListener('click', function(){
+    const positionX = mouse.x - (mouse.x % cellSize);
+    const positionY = mouse.y - (mouse.y % cellSize);
+    
+    //To prevent setting a plant in the resource area
+    if(positionY < cellSize)return;
+    let plantCost = 60;
+
+    //creation of an empty plant object based on resources available
+    if(resources > plantCost){
+        plant.push(new plants(positionX, positionY));
+        resources -= plantCost; 
+    }
+})
